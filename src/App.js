8d52a5f6399027e1146c2c15f1dd16aaffbcab19
Manu M <manumohan.cvav@gmail.com>
2019-07-14 15:39:02 +0530

    Preparing for deployment

--- src/App.js
+++ src/App.js
@@ -48,6 +48,7 @@ class App extends Component {
     })
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox placeholder='Search monster' handleChange={this.handleChange}/>
         <CardList monsters={filteredMonsters}/>
       </div>
