c032a3059223c433ca1f98a9266578731946eafb
Manu M <manumohan.cvav@gmail.com>
2019-08-03 16:19:10 +0530

    Updates

---
+++ precache-manifest.92f60474d776ea3a90bc51e06000f050.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dd5924ecb786e2b5487c75761a75db1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3233505b941f9fabb4a2",
+    "url": "/monsters-rolodex/static/css/main.605a71af.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3233505b941f9fabb4a2",
+    "url": "/monsters-rolodex/static/js/main.591ab03b.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
